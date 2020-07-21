3693beb9099346ac8c8f32919b54ecc259b32423
hebiao <hebiao@orientsec.com.cn>
2020-07-22 01:00:17 +0800

    【税如意系统】后端-客户充值文件调试

--- backend/tz-tavern/tavern-app/tavern-shuiruyi/src/main/java/com/tuozuo/tavern/shuiruyi/service/impl/CustomInfoServiceImpl.java
+++ backend/tz-tavern/tavern-app/tavern-shuiruyi/src/main/java/com/tuozuo/tavern/shuiruyi/service/impl/CustomInfoServiceImpl.java
@@ -35,7 +35,7 @@ import java.util.List;
 @Service
 public class CustomInfoServiceImpl implements CustomInfoService {
 
-    @Value("${shuiruyi.file.url.path:http://119.3.19.171/shuiruyi/file/}")
+    @Value("${shuiruyi.file.url.path:http://119.3.19.171/shuiruyi/file/trade/file/}")
     private String tradeFileUrlPath;
     @Value("${shuiruyi.custom.trade.file.path:/mnt/file/trade/file/}")
     private String tradeFilePath;
