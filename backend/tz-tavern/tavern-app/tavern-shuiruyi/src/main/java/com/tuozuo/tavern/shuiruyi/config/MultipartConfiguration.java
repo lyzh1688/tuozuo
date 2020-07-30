9b8999bb3d8731a9be3e24522e671f4177300ab7
hebiao <343675896@qq.com>
2020-07-30 18:02:22 +0800

    【税如意系统】后端-增加文件大小控制

---
+++ backend/tz-tavern/tavern-app/tavern-shuiruyi/src/main/java/com/tuozuo/tavern/shuiruyi/config/MultipartConfiguration.java
@@ -0,0 +1,22 @@
+package com.tuozuo.tavern.shuiruyi.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.MultipartConfigElement;
+
+/**
+ * Code Monkey: 何彪 <br>
+ * Dev Time: 2020/7/30 <br>
+ */
+@Configuration
+public class MultipartConfiguration {
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("30MB");
+        factory.setMaxRequestSize("30MB");
+        return factory.createMultipartConfig();
+    }
+}
