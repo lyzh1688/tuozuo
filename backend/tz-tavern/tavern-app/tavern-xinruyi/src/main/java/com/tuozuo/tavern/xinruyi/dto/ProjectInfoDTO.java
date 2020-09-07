438b9d933e63037a74807e2235a7f7d32ccf8895
hebiao <hebiao@orientsec.com.cn>
2020-09-07 23:53:55 +0800

    【薪如意系统】后端-项目列表开发

--- backend/tz-tavern/tavern-app/tavern-xinruyi/src/main/java/com/tuozuo/tavern/xinruyi/dto/ProjectInfoDTO.java
+++ backend/tz-tavern/tavern-app/tavern-xinruyi/src/main/java/com/tuozuo/tavern/xinruyi/dto/ProjectInfoDTO.java
@@ -7,8 +7,9 @@ import java.math.BigDecimal;
  * Dev Time: 2020/8/30 <br>
  */
 public class ProjectInfoDTO {
-    private String projectId;
+    private String companyId;
     private String companyName;
+    private String projectId;
     private String projectName;
     private String projectCycle;
     private BigDecimal budget;
@@ -16,6 +17,25 @@ public class ProjectInfoDTO {
     private String beginDate;
     private String endDate;
     private String projectStatus;
+
+  /*  companyId	String	公司Id
+    companyName	String	公司名称
+    projectId	String	项目Id
+    projectName	String	项目名称
+    applyDate	String	申请日期
+    projectStatus	String	申请状态
+    rate	number	服务费率
+    industryType	String	行业分类
+    projectCycle	String	项目周期
+    releaseDate	String	发布日期
+    staffNum	number	项目人员
+    city	String	所在城市	字典？
+    contact	String	联系电话
+    isOnSite	String	是否驻场	0，否，1，是
+    contactName	String	项目联系人
+    projectFile	String	项目资料
+    remark	String	备注*/
+
 
     public String getProjectId() {
         return projectId;
