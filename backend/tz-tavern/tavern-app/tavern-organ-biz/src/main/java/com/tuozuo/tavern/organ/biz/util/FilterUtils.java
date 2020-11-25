01a077a121701be0cff0a0e2d67c3a7c137ece6e
hebiao <343675896@qq.com>
2020-11-25 21:49:20 +0800

    【起名核名系统】封装企查查核名流程

--- backend/tz-tavern/tavern-app/tavern-organ-biz/src/main/java/com/tuozuo/tavern/organ/biz/util/FilterUtils.java
+++ backend/tz-tavern/tavern-app/tavern-organ-biz/src/main/java/com/tuozuo/tavern/organ/biz/util/FilterUtils.java
@@ -4,6 +4,7 @@ import com.tuozuo.tavern.organ.biz.store.CompanyPropertyStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,17 +18,34 @@ public class FilterUtils {
     @Autowired
     private CompanyPropertyStore companyPropertyStore;
 
-    public String filterSpecialChar(String str){
-        String regEx="[`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+    public String filterSpecialChar(String str) {
+        String regEx = "[`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
         Pattern p = Pattern.compile(regEx);
         Matcher m = p.matcher(str);
         return m.replaceAll("").trim();
     }
 
-    public String filterAreaChar(String str){
+    public List<String> filterAreaChar(List<String> str) {
+        for (int i = 0; i < str.size(); i++) {
+            if (companyPropertyStore.getAreaMap().containsKey(str.get(i))) {
+                str.remove(i);
+            }
+        }
 
+        return str;
     }
+    public List<String> filterTypeChar(List<String> str) {
+        for (int i = 0; i < str.size(); i++) {
+            if (companyPropertyStore.getTypeMap().containsKey(str.get(i))) {
+                for (int j = i; j < str.size(); j++) {
+                    str.remove(j);
 
+                }
+            }
+        }
+
+        return str;
+    }
 
 
 }
